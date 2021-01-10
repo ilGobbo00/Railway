f56ccf9520f042375ca33704cd4abf22805c80db
Riccardo Gobbo <riccardogobbo00@gmail.com>
2021-01-10 17:07:21 +0100

    Modifiche di prova

--- mainRailway.cpp
+++ mainRailway.cpp
@@ -1,5 +1,8 @@
 #include "Railway.h"
 int main(){
-
+    Railway sistema("", "");
+    Principal start("Treviso", 50, nullptr, sistema);
+    Regional test("1234", false, vector<int>, start, sistema);
+    Regional dioporco()
     return 0;
 }
