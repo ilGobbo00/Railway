970d02faaa05d53b9b3e1a6a09d81800f8e8e91c
Giulio-Rebecchi <76965569+Giulio-Rebecchi@users.noreply.github.com>
2021-01-12 08:46:11 +0100

    Updated to final(?) version

--- mainRailway.cpp
+++ mainRailway.cpp
@@ -1,8 +1,12 @@
 #include "Railway.h"
-int main(){
-    Railway sistema("", "");
-    Principal start("Treviso", 50, nullptr, sistema);
-    Regional test("1234", false, vector<int>, start, sistema);
-    Regional dioporco()
-    return 0;
+
+using std::cout;
+
+int main()
+{
+    cout << "--- RAILWAY ---\n";
+	Railway rail_system("line_description", "timetables");
+	while (!rail_system.is_completed())
+		rail_system.advance_time();
+	return 0;
 }
